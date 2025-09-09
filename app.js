c4191ecadc3d28c1226730507c38c37bb3ab73f5
Dee McDonald <dee.mcdonald31@gmail.com>
2025-09-08 20:25:54 -0500

    Updated app.js to display the operator type in the submit button

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 let mainForm = document.getElementById('mainForm');
+let subBtn = document.getElementById('submit');
 
 mainForm.addEventListener('submit', addNumbers)
-
+mainForm.addEventListener('change', changeOperator)
 
 function addNumbers(e){
     e.preventDefault();
@@ -11,38 +12,46 @@ function addNumbers(e){
     let getNum2 = document.getElementById('num2').value;
     let intNum1 = parseInt(getNum1, 10);
     let intNum2 = parseInt(getNum2, 10);
-    e.preventDefault();
+
     if(isNaN(intNum1) || isNaN(intNum2) || operator === "0"){
-        console.log('This is not a valid number')
         output.innerHTML  = /* html */ `
             <span class="error">Please enter valid numbers</span>
             <span class="error">Please select a math operator</span>
         `;
-        console.log(operator)
-        console.log(typeof(intNum1))
-        console.log(typeof(intNum2))
     }else{
-        console.log(typeof(intNum1))
-        console.log(typeof(intNum2))
-        console.log(intNum1)
-        console.log(intNum2)
         if(operator === 'add'){
         output.innerHTML = /* html */ `
             <span class="success">${intNum1}  +  ${intNum2} =  ${intNum1 + intNum2}</span>
         `;
         }else if(operator === 'subtract'){
         output.innerHTML = /* html */ `
-            <span class="success">${intNum1}  -  ${intNum2} =  ${intNum1 - intNum2}</span>
+        <span class="success">${intNum1}  -  ${intNum2} =  ${intNum1 - intNum2}</span>
         `;
         }else if(operator === 'multiply'){
         output.innerHTML = /* html */ `
-            <span class="success">${intNum1}  x  ${intNum2} =  ${intNum1 * intNum2}</span>
+        <span class="success">${intNum1}  x  ${intNum2} =  ${intNum1 * intNum2}</span>
         `;
         }else if(operator === 'divide'){
-            let intDivinde = intNum1 / intNum2;
+        let intDivinde = intNum1 / intNum2;
         output.innerHTML = /* html */ `
-            <span class="success">${intNum1}  ∻  ${intNum2} =  ${intDivinde.toFixed(2)}</span>
+        <span class="success">${intNum1}  ∻  ${intNum2} =  ${intDivinde.toFixed(2)}</span>
         `;
         }
+    }
+}
+
+
+function changeOperator(){
+    let operator = document.getElementById('operator').value;
+    if(operator === 'add'){
+        subBtn.textContent = 'Add Numbers';
+    }else if(operator === 'subtract'){
+        subBtn.textContent = 'Subtract Numbers';
+    }else if(operator === 'divide'){
+        subBtn.textContent = 'Divide Numbers';
+    }else if(operator === 'multiply'){
+        subBtn.textContent = 'Multiply Numbers';
+    }else{;
+        subBtn.textContent = 'Add Numbers'
     }
 }
